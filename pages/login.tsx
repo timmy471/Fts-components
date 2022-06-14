981f2fdb0afd71c59fe767bba7ec613a8bea112f
jideabdqudus <jideabdqudus@gmail.com>
2022-06-14 21:04:30 +0100

    WIP: Login Page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,4 @@
-import { Layout, Row, Col, Input } from 'antd';
+import { Layout, Row, Col, Button } from 'antd';
 import type { CheckboxChangeEvent } from 'antd/es/checkbox';
 import type { NextPage } from 'next';
 import Head from 'next/head';
@@ -53,22 +53,31 @@ const Login: NextPage<Props> = () => {
         </Fragment>
         <div>
           <Row>
-            <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+            <Col xs={24} sm={24} md={14} lg={14} xl={14}>
               <div className='left-hero'>
                 <div className='image-wrapper'>
-                  <Image
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
                     src={assets.LoginHero.src}
                     alt={assets.LoginHero.alt}
-                    // layout='fill'
-                    // objectFit='contain'
                     width='100%'
-                    height={'100%'}
+                    height='100%'
                   />
                 </div>
+                <p>
+                  Bridging the gap between the best of Africa’s <br />
+                  startups and investors
+                </p>
               </div>
             </Col>
 
-            <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+            <Col
+              xs={24}
+              sm={24}
+              md={10}
+              lg={10}
+              xl={10}
+              className='d-flex justify-content-center'>
               <div className='right-hero'>
                 <h1>Welcome back</h1>
                 <h6>
@@ -82,6 +91,27 @@ const Login: NextPage<Props> = () => {
                     onFormChange={onFormChange}
                     onRememberCheck={onRememberCheck}
                   />
+                </div>
+                <div className='other-socials-login'>
+                  <Row>
+                    <Col xs={6} sm={6} md={6} lg={6} xl={6} style={{ margin: 'auto' }}>
+                      <hr />
+                    </Col>
+                    <Col xs={12} sm={12} md={12} lg={12} xl={12} className='text-center'>
+                      Login with other accounts
+                    </Col>
+                    <Col xs={6} sm={6} md={6} lg={6} xl={6} style={{ margin: 'auto' }}>
+                      <hr />
+                    </Col>
+                  </Row>
+                  <Row gutter={[16, 16]}>
+                    <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                      <Button className='btn-social'>Google</Button>
+                    </Col>
+                    <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                      <Button className='btn-social'>LinkedIn</Button>
+                    </Col>
+                  </Row>
                 </div>
               </div>
             </Col>
