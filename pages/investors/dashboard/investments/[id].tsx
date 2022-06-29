8d8e94953a296486a6ae2a2c6165d387e1f6cefe
jideabdqudus <jideabdqudus@gmail.com>
2022-06-29 13:26:48 +0100

    WIP: Individual Active Page

--- pages/investors/dashboard/investments/[id].tsx
+++ pages/investors/dashboard/investments/[id].tsx
@@ -23,7 +23,7 @@ const InvestorsDashboard: NextPage<IProps> = () => {
         </span>
         <div className='image-cta' style={{ backgroundImage: `url(${assets.DealCta.src})` }}>
           <Row className='deal-name'>
-            <Col xs={24} sm={24} md={3} lg={3} xl={3}>
+            <Col xs={3} sm={3} md={3} lg={3} xl={3}>
               <Image
                 src={assets.PinterestLogo.src}
                 alt={assets.PinterestLogo.alt}
@@ -31,8 +31,8 @@ const InvestorsDashboard: NextPage<IProps> = () => {
                 height={'100'}
               />
             </Col>
-            <Col xs={24} sm={24} md={14} lg={14} xl={14}>
-              <h1>Future Africa Fund II</h1>
+            <Col xs={18} sm={18} md={14} lg={14} xl={14}>
+              <h1>Future Africa</h1>
             </Col>
             <Col xs={24} sm={24} md={5} lg={5} xl={5}>
               <Button className='btn-invest'>Invest</Button>
